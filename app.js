70d984c01a305bf65c926c7bd828af49e84f7bd7
YangHang <sidney.hang.y@gmail.com>
2018-11-13 23:47:01 +0800

    获取微信用户信息

--- app.js
+++ app.js
@@ -6,19 +6,30 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+    var that = this;
+
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: that.globalData.wx_url_1 + res.code + that.globalData.wx_url_2,
+          success: res => {
+            console.log(res);
+            that.globalData.openid = res.data.openid;
+          }
+        })
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log(res);
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log(res);
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
@@ -29,11 +40,18 @@ App({
               }
             }
           })
+        } else {
+          wx.navigateTo({
+            url: '/pages/login/login',
+          })
         }
       }
     })
   },
   globalData: {
-    userInfo: null
+    openid: 0,
+    wx_url_1: 'https://api.weixin.qq.com/sns/jscode2session?appid=wxa30da721db587413&secret=68bcec49a3919e51521c9c02a1fcdba2&js_code=',
+    wx_url_2: '&grant_type=authorization_code',
+    urlPath: 'http://192.168.31.107:9000/expresslove/api/v1'
   }
 })
