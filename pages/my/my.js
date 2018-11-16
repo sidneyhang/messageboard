22aa438bc55d1afeb10080812ed448f75ac55a75
dayue12168 <dayue12168@163.com>
2018-11-16 23:42:41 +0800

    签到

--- pages/my/my.js
+++ pages/my/my.js
@@ -10,7 +10,8 @@ Page({
     avatarUrl: "",
     nickName: "",
     flowerCount: 0,
-    eggCount: 0
+    eggCount: 0,
+    modalShow: false
   },
 
   onLoad: function() {
@@ -31,7 +32,38 @@ Page({
     wx.navigateTo({
       url: '/pages/my/pay/pay',
     })
+  },
+  signModal: function(e) {
+    this.setData({
+      modalShow: true
+    })
+  },
+  closeModal: function(e) {
+    wx.request({
+      url: app.globalData.urlPath + "/signin",
+      method: "POST",
+      header: {
+        "Authorization": app.globalData.access_token
+      },
+      success: res => {
+        console.log(res);
+        if (res.data.code === 200) {
+          this.setData({
+            modalShow: false
+          })
+        } else {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'none',
+            duration: 2000
+          });
+          this.setData({
+            modalShow: false
+          })
+          return false;
+        }
+      }
+    })
   }
-
 
 })
